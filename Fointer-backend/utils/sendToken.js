cc74d844be1fe658c7d2fbe3636b8ad031c40ced
vandana shakya <vandanashakya1087@gmail.com>
2026-08-01 15:11:26 +0530

    cookie set again

--- Fointer-backend/utils/sendToken.js
+++ Fointer-backend/utils/sendToken.js
@@ -9,7 +9,7 @@ const sendToken = (user, statusCode, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: true,
-    sameSite: "lax",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
